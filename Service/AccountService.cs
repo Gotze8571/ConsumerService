73b543e1e01845552a64684c9a72b4104f18c1e6
Tella Adesina <telladesina100@gmail.com>
2022-03-09 14:24:57 -0600

    Add-New-Services

---
+++ Service/AccountService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerService.Service
+{
+    // Service checks account balance, Account fraud, Account Expenditure rate, Customer Commission rate 
+    public class AccountService
+    {
+        // Check Account balance every 5 mins interval
+        public void CheckAccountBalance()
+        {
+        }
+
+        public void CheckAccountFraud()
+        {
+
+        }
+    }
+}
---
+++ Service/ValidateCustomerDetailsService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerService.Service
+{
+    // Check customer profile for fraud, 
+    public class ValidateCustomerDetailsService
+    {
+        // Check if Customer commited fraud or crime
+        public void CheckCustomerProfile()
+        {
+
+        }
+
+        // Check if Customer Relative Commited Fraud
+        public void CheckCustomerRelativeProfile()
+        {
+
+        }
+
+        // Check if Customer Eligible for loan
+        public void CheckCustomerEligiblity()
+        {
+
+        }
+
+
+    }
+}
